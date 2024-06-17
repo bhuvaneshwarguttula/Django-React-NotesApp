44fa92371c31c72e18b2a31b408ab5ebceaef8dc
bhuvaneshwarguttula <bhuvaneshwarguttula@gmail.com>
2024-06-17 13:38:06 +0530

    Changed settings

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -129,6 +129,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'frontend/build/static'
 ]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
